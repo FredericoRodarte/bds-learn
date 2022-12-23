4e254ba5a6ad69f266b7fb9939dfdf472473e4a3
Frederico Rodarte <fredericorodarte12@gmail.com>
2022-12-23 10:51:29 -0300

    offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -19,3 +19,7 @@ INSERT INTO tb_course (name,img_uri,img_gray_uri)
 VALUES ('BootCamp HTML',
 'https://upload.wikimedia.org/wikipedia/commons/e/e5/Dom-Pedro-Old-Golf-Course-Vilamoura.jpg',
 'https://live.staticflickr.com/2891/11619047474_71850d7f16_b.jpg');
+
+INSERT INTO TB_OFFER(edition,start_Moment,end_Moment,course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z',1);	
+
+INSERT INTO TB_OFFER(edition,start_Moment,end_Moment,course_id) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z',1);	              
