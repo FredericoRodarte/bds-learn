c679f8b787d81908ced18918a7e11f4e86ea4bcf
Frederico Rodarte <frederiorodarte12@gmail.com>
2024-07-05 17:03:35 -0300

    Fim do Capitulo 5

--- backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslearnbds.dto.NotificationDTO;
@@ -19,8 +19,10 @@ public class NotificationResource {
 	private NotificationService service;
 	
 	@RequestMapping		
-	private ResponseEntity<Page<NotificationDTO>> notificationsForCurrentUser(Pageable pageable) {
-		Page<NotificationDTO> page = service.notificationsForCurrentUser(pageable);
+	private ResponseEntity<Page<NotificationDTO>> notificationsForCurrentUser(
+			@RequestParam(name = "unreadOnly",defaultValue = "false") Boolean unreadOnly,
+			Pageable pageable) {
+		Page<NotificationDTO> page = service.notificationsForCurrentUser(unreadOnly,pageable);
 		return ResponseEntity.ok().body(page);
 	}
 }
